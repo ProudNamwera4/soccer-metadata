f51aa90efeb2e6d37ad0c5bc33c5d2224ae17956
ProudNamwera4 <proudnamwera4@gmail.com>
2024-02-19 08:42:21 +0200

    gh header and footer

--- src/js/util.mjs
+++ src/js/util.mjs
@@ -23,9 +23,9 @@ export async function loadTemplate(path) {
   }
 
   export async function loadHeaderFooter(path) {
-    const headerTemplate = await loadTemplate("../public/partials/header.html");
+    const headerTemplate = await loadTemplate("./public/partials/header.html");
     const headerElement = document.querySelector("#main-header");
-    const footerTemplate = await loadTemplate("../public/partials/footer.html");
+    const footerTemplate = await loadTemplate("./public/partials/footer.html");
     const footerElement = document.querySelector("#main-footer");
   
     renderWithTemplate(headerTemplate, headerElement);
