5746b36ab9d91900287aee17e1ff6836c2e4a5a1
ProudNamwera4 <proudnamwera4@gmail.com>
2024-02-19 08:20:53 +0200

    fetch and render the leagues page

---
+++ src/js/players.js
@@ -0,0 +1,1 @@
+const url = 'www.thesportsdb.com/api/v1/json/3/searchplayers.php?p=Danny_Welbeck'
---
+++ src/js/util.mjs
@@ -0,0 +1,42 @@
+
+// function to take a list of objects and a template and insert the objects as HTML into the DOM
+export function renderListWithTemplate(
+    templateFn,
+    parentElement,
+    list,
+    position = "afterbegin",
+    clear = false
+  ) {
+    const htmlStrings = list.map(templateFn);
+    // if clear is true we need to clear out the contents of the parent.
+    if (clear) {
+      parentElement.innerHTML = "";
+    }
+    parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+  }
+
+//Load,Grab and Render the header and footer
+export async function loadTemplate(path) {
+    const res = await fetch(path);
+    const template = await res.text();
+    return template;
+  }
+
+  export async function loadHeaderFooter(path) {
+    const headerTemplate = await loadTemplate("../partials/header.html");
+    const headerElement = document.querySelector("#main-header");
+    const footerTemplate = await loadTemplate("../partials/footer.html");
+    const footerElement = document.querySelector("#main-footer");
+  
+    renderWithTemplate(headerTemplate, headerElement);
+    renderWithTemplate(footerTemplate, footerElement);
+  }
+
+  //load the templates
+export function renderWithTemplate(template, parentElement, data, callback) {
+    parentElement.insertAdjacentHTML("afterbegin", template);
+    //if there is a callback...call it and pass data
+    if (callback) {
+      callback(data);
+    }
+  }
